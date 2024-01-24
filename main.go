9892370c6f9eb4566356fc53ce8b059c61e6a942
Violet Guo <violetguos@users.noreply.github.com>
2024-01-24 17:34:13 -0500

    manual cmt

--- main.go
+++ main.go
@@ -1,75 +1,48 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"os"
-	"strings"
-	"golang.org/x/net/html"
+    "strings"
+    "fmt"
 )
 
-var raw = ` <!DOCTYPE html> <html>
-  <body>
-    <h1>My First Heading</h1>
-      <p>My first paragraph.</p>
-      <p>HTML images are defined with the img tag:</p>
-      <img src="xxx.jpg" width="104" height="142">
-</body> </html>`
-
-func helper(curr *html.Node, numWords int, numImg int) (int, int) {
-    // my solution with return vals
-    if curr.Type == html.ElementNode && curr.Data == "img" {
-        for _, a := range curr.Attr {
-            if a.Key == "src" {
-                numImg++
-                
+func letterCombinations(digits string) []string {
+    numToChar := map[string]string{
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    }
+    result := []string{}
+    if len(digits) == 0 {
+        return result
+    }
+    for i := range digits {
+        if len(result) == 0 {
+            fmt.Println(digits[i])
+            chars := strings.Split(numToChar[digits[i]], "")
+            for _, c := range chars {
+                result = append(result, c)
             }
+        } else {
+            curr_result := []string{}
+            for j := range result {
+                chars := strings.Split(numToChar[digits[i]], "")
+                for _, c := range chars {
+                    curr_result = append( append(result[j], c))
+                }
+            }
+            result = curr_result
         }
-    } else if curr.Type == html.TextNode {
-            
-            numWords+=len(strings.Fields(curr.Data))
-        
     }
-    for next := curr.FirstChild; next != nil; next = next.NextSibling {
-        fmt.Printf("curr %s, next %s\n", curr.Data, next.Data)
-        numWords, numImg = helper(next, numWords, numImg)
-    }
-    return numWords, numImg
+    return result
 
-}
-
-func visit(n *html.Node, words, pics *int) {
-    // official solution
-    if n.type == html.TextNode{
-        *words += len(strings.Fields(n.Data))
-    } else if n.Type == html.ElementNode && n.Data == "img" {
-        *pics++
-    }
-
-    for c := n.FirstChild; c !=nil; c = c.NextSibling {
-        visit(c, words, pics)
-    }
-
-
-}
-
-func countWordsAndImages(doc *html.Node) (int, int) {
-	
-    // count all nodes in the current level
-	return helper(doc, 0, 0)
-
-    // using official solution
-    // var pics, words int
-    // visit(doc, &words, &pics)
-    // return words, pics
 }
 
 func main() {
-	doc, err := html.Parse(bytes.NewReader([]byte(raw)))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "parse failed: %s\n", err)
-		os.Exit(-1)
-	}
-	words, pics := countWordsAndImages(doc)
-	fmt.Printf("%d words and %d images\n", words, pics)
+
+	fmt.Println(letterCombinations("23"))
 }
--- practice.go
+++ practice.go
@@ -1,3 +1,8 @@
+/*
+This is not intended to run out of the box
+The individual funcs are tested in Leetcode
+*/
+
 func partitionLabels(s string) []int {
     // https://leetcode.com/problems/partition-labels/description/
 	// beats 100% 
@@ -20,3 +25,55 @@ func partitionLabels(s string) []int {
     }
     return partitions
 }
+
+// Begin: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+type Key struct {
+    X, Y int
+}
+
+func maxProfit(prices []int) int {
+    cache := make(map[Key]int)
+    return helper(prices, 0, 0, cache)
+}
+
+func helper(prices []int, i int, op int, cache map[Key]int) int {
+    const BUY = 0
+    const SELL = 1
+    if i>= len(prices) {
+        cache[Key{i, op}] = 0
+        return 0
+    }
+    if _, ok := cache[Key{i, op}]; ok {
+        return cache[Key{i, op}]
+    }
+    var p int
+    if op == BUY{
+        p = max(helper(prices, i+1, SELL, cache)- prices[i], helper(prices, i+1, BUY, cache))
+    } else if op == SELL{
+        p = max(helper(prices, i+2, BUY, cache) + prices[i],
+        helper(prices, i+1, SELL, cache))
+    }
+    cache[Key{i, op}] = p 
+    return cache[Key{i, op}]
+}
+// END: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+
+// BEGIN: https://leetcode.com/problems/daily-temperatures
+func dailyTemperatures(temperatures []int) []int {
+    stack := make([][2]int, 0)
+    result := make([]int, len(temperatures)) //default 0
+
+    for i, temp := range temperatures {
+        for len(stack) > 0 && stack[len(stack)-1][1] < temp {
+            prev := stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            result[prev[0]] = i - prev[0]
+        }
+        stack = append(stack, [2]int{i, temp})
+
+    }
+    return result
+
+}
+
+// END: https://leetcode.com/problems/daily-temperatures
